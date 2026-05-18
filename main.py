6838945ec46dfd8dd0cd2fa27a1e48578d2b11ab
Otto de Maré <otto.demare@alpcot.se>
2026-05-18 11:26:07 +0200

    changed directory to Downloads

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
 from pathlib import Path
 
-dir_path = Path('/home/ottodm/TestingFolder/')
+dir_path = Path('/home/ottodm/Downloads/')
 
 for files in dir_path.glob('*.txt'):
-    folder_name = Path('/home/ottodm/TestingFolder/texts/')
+    folder_name = Path('/home/ottodm/Downloads/texts/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.md'):
-    folder_name = Path('/home/ottodm/TestingFolder/texts/')
+    folder_name = Path('/home/ottodm/Downloads/texts/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.mp4'):
-    folder_name = Path('/home/ottodm/TestingFolder/movies/')
+    folder_name = Path('/home/ottodm/Downloads/movies/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.mp3'):
-    folder_name = Path('/home/ottodm/TestingFolder/music/')
+    folder_name = Path('/home/ottodm/Downloads/music/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.py'):
-    folder_name = Path('/home/ottodm/TestingFolder/pythonProjects/')
+    folder_name = Path('/home/ottodm/Downloads/pythonProjects/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.png'):
-    folder_name = Path('/home/ottodm/TestingFolder/images/')
+    folder_name = Path('/home/ottodm/Downloads/images/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.jpg'):
-    folder_name = Path('/home/ottodm/TestingFolder/images/')
+    folder_name = Path('/home/ottodm/Downloads/images/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.pdf'):
-    folder_name = Path('/home/ottodm/TestingFolder/pdf/')
+    folder_name = Path('/home/ottodm/Downloads/pdf/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
 for files in dir_path.glob('*.zip'):
-    folder_name = Path('/home/ottodm/TestingFolder/zip/')
+    folder_name = Path('/home/ottodm/Downloads/zip/')
     folder_name.mkdir(parents=True, exist_ok=True)
     files.rename(dir_path / folder_name / files.name)
 
