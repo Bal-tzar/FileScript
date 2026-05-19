0e0a5507c0b5480f9fe6e7d58fa6a9959c1ff1f7
Otto de Maré <otto.demare@alpcot.se>
2026-05-19 11:32:46 +0200

    Changed names, and naming convention. Refactored files.rename

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import json
 with(open('config.json')) as config_file:
     data = json.load(config_file)
 
-cleanUp = input("Which directory would you like to clean up? ")
-dir_path = Path(cleanUp)
-print("--------- These are the unsorted files in ", cleanUp, "---------")
+target_directory = input("Which directory would you like to clean up? ")
+dir_path = Path(target_directory)
+print("--------- These are the unsorted files in ", target_directory, "---------")
 for item in dir_path.glob("*"):
     if item.is_file():
         print(item)
 
 
-sortingChoice = input("Would you like to sort these files? yes/no:   ")
+sorting_choice = input("Would you like to sort these files? yes/no:   ")
 
 
-if sortingChoice == 'yes':
+if sorting_choice == 'yes':
     for files in dir_path.glob('*'):
         if files.is_file():
             extension = files.suffix
             if extension in data:
                 folder_name = data[extension]
-                destination = Path(cleanUp, folder_name)
+                destination = Path(target_directory, folder_name)
                 destination.mkdir(parents=True, exist_ok=True)
-                files.rename(dir_path / folder_name / files.name)
+                files.rename(destination / files.name)
     print('Cleaned up in', dir_path)
 
 else:
